09d61b9674c724dfec65c9208e38159689efcd64
Rina Kadishani <kadishanirina@gmail.com>
2026-04-15 09:58:51 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -6,19 +6,3 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('Home');
 })->name('home');
-
-Route::get('/login', function () {
-    return Inertia::render('Auth/Login');
-})->name('login');
-
-Route::get('/register', function () {
-    return Inertia::render('Auth/Register');
-})->name('register');
-
-Route::get('/jobs', function () {
-    return Inertia::render('Jobs/Index');
-})->name('jobs');
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard/Index');
-})->name('dashboard');
