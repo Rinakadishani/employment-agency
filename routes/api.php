01be55b81f785e03e1db00a51f29ceafdc716a85
Rina Kadishani <kadishanirina@gmail.com>
2026-04-17 11:47:45 +0200

    VendiPunesController CRUD API with filters and authorization

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,29 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\VendiPunesController;
 
+// Public auth routes
 Route::prefix('auth')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
-    Route::post('login', [AuthController::class, 'login']);
-    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('login',    [AuthController::class, 'login']);
+    Route::post('refresh',  [AuthController::class, 'refresh']);
 });
 
+// Protected auth routes
 Route::prefix('auth')->middleware('auth:api')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
-    Route::get('me', [AuthController::class, 'me']);
+    Route::get('me',      [AuthController::class, 'me']);
+});
+
+// Public job routes
+Route::get('vendet-punes',          [VendiPunesController::class, 'index']);
+Route::get('vendet-punes/{vendiPunes}', [VendiPunesController::class, 'show']);
+
+// Protected job routes
+Route::middleware('auth:api')->group(function () {
+    Route::post('vendet-punes',             [VendiPunesController::class, 'store']);
+    Route::put('vendet-punes/{vendiPunes}',  [VendiPunesController::class, 'update']);
+    Route::delete('vendet-punes/{vendiPunes}', [VendiPunesController::class, 'destroy']);
 });
